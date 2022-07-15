00d6d8abf82b41ffc3cc3362ebb601e468f0921f
Jacob Wynne <jacobwynne@vt.edu>
2022-07-15 12:17:30 -0400

    updating scenario nmls to output in external drive

--- Scripts/altering_inflows/run_alteredinflows.R
+++ Scripts/altering_inflows/run_alteredinflows.R
@@ -3,10 +3,13 @@ rm(list = ls())
 pacman::p_load(tidyverse, lubridate, ncdf4, GLMr, glmtools)
 sim_folder <- getwd()
 
+setwd('Users/jacobwynne/Dropbox/SUNP-GLMv3.3-JHW/')
 # Make sure the most recent version of glm3.nml is copied to glm4.nml
 file.copy('glm3.nml', 'glm4.nml', overwrite = TRUE)
 
+#setwd("/Volumes/G-DRIVE SSD/scenario_output")
 
+getwd()
 
 file.copy('scenario_nmls/glm3_i505.nml', 'glm3.nml', overwrite = TRUE)
 system2("/Users/jacobwynne/Dropbox/SUNP-GLMv3.3-JHW/glm.app/Contents/MacOS/glm", stdout = TRUE, stderr = TRUE, env = "DYLD_LIBRARY_PATH=/Users/jacobwynne/Dropbox/SUNP-GLMv3.3-JHW/glm.app/Contents/MacOS")
